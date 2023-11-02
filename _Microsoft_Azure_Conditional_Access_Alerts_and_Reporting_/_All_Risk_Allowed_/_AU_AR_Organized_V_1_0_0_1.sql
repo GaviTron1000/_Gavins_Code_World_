9401de4fb2fd08905a627e619a2de34443804653
GaviTron1000 <142179470+GaviTron1000@users.noreply.github.com>
2023-11-02 15:15:29 -0400

    basic

--- _Microsoft_Azure_Conditional_Access_Alerts_and_Reporting_/_All_Risk_Allowed_/_AU_AR_Organized_V_1_0_0_1.sql
+++ _Microsoft_Azure_Conditional_Access_Alerts_and_Reporting_/_All_Risk_Allowed_/_AU_AR_Organized_V_1_0_0_1.sql
@@ -1,32 +1,30 @@
-Let dataFiltrationStep1 = view() {
+let dataFiltrationStep1 = view() {
     AADRiskyUsers
-    | where not(RiskLevel has_any("medium", "hidden", "high", "none", "unknownFutureValue"))
-    | where not (RiskLevel has_any("low"))
+    | where not(RiskLevel has_any("medium", "high", "none", "unknownFutureValue"))
+    | where (RiskLevel has_any("low"))
     | where not(RiskState has_any("none", "confirmedSafe", "remediated", "dismissed", "unknownFutureValue"))
-    | where RiskState has_any("atRisk", "confirmedCompromised")
+    | where (RiskState has_any("atRisk", "confirmedCompromised"))
     | where TimeGenerated > ago(30m) and TimeGenerated < ago(10m)
-    | summerise count() by UserPrincipalName
+    | summarize count() by UserPrincipalName
+    | take 1
+    };
+let dataFiltrationStep2 = view() {
+    AADRiskyUsers
+    | where not(RiskLevel has_any("low", "high", "none", "unknownFutureValue"))
+    | where (RiskLevel has_any("medium"))
+    | where (RiskState has_any("atRisk", "confirmedCompromised"))
+    | where TimeGenerated > ago(30m) and TimeGenerated < ago(10m)
+    | summarize count() by UserPrincipalName
     | take 1
 };
-Let dataFiltrationStep2 = view() {
-  AADRiskyUsers
-  | where not(RiskLevel has_any("low", "hidden", "high", "none", "unknownFutureValue"))
-  | where not(RiskLevel has_any("medium"))
-  | where not(RiskState has_any("none", "confirmedSafe", "remediated", "dismissed", "unknownFutureValue"))
-  | where RiskState has_any("atRisk", "confirmedCompromised")
-  | where TimeGenerated > ago(30m) and TimeGenerated < ago(10m)
-  | summarize count() by UserPrincipalName
-  | take 1
-};
-Let dataFiltrationStep3 = view() {
-  AADRiskyUsers
-  | where not(RiskLevel has_any("medium", "hidden", "low", "none", "unknownFutureValue"))
-  | where not (RiskLevel has_any("high"))
-  | where not(RiskState has_any("none", "confirmedSafe", "remediated", "dismissed", "unknownFutureValue"))
-  | where RiskState has_any("atRisk", "confirmedCompromised")
-  | where TimeGenerated > ago(30m) and TimeGenerated < ago(10m)
-  | summarize count() by UserPrincipalName
-  | take 1
+let dataFiltrationStep3 = view() {
+    AADRiskyUsers
+    | where not(RiskLevel has_any("low", "medium", "none", "unknownFutureValue"))
+    | where (RiskLevel has_any("high"))
+    | where (RiskState has_any("atRisk", "confirmedCompromised"))
+    | where TimeGenerated > ago(30m) and TimeGenerated < ago(10m)
+    | summarize count() by UserPrincipalName
+    | take 1
 };
 let dataFiltrationStep4 = view() {
     SigninLogs
@@ -40,4 +38,4 @@ let dataFiltrationStep4 = view() {
     | where CreatedDateTime > ago(30m) and CreatedDateTime < ago(10m)
     | project CreatedDateTime, UserPrincipalName, UserType, ResourceDisplayName, AuthenticationRequirement, AuthenticationDetails, IPAddress, DeviceDetail, LocationDetails, Location, Status, ConditionalAccessPolicies, ConditionalAccessStatus, RiskDetail
 };
-union withsource = "allRiskyUsers_and_policyFailures" dataFiltrationStep1, dataFiltrationStep2, dataFiltrationStep3, dataFiltrationStep4
+union withsource = "allOutput" dataFiltrationStep1, dataFiltrationStep2, dataFiltrationStep3, dataFiltrationStep4
