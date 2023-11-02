5d7d49ef343b06035223137f98fde4d6db73543f
GaviTron1000 <142179470+GaviTron1000@users.noreply.github.com>
2023-11-02 15:14:34 -0400

    Corrected some syntax. last update on this version

--- _Microsoft_Azure_Conditional_Access_Alerts_and_Reporting_/_All_Risk_Allowed_/_AU_AR_Organized_V_1_0_0_0.sql
+++ _Microsoft_Azure_Conditional_Access_Alerts_and_Reporting_/_All_Risk_Allowed_/_AU_AR_Organized_V_1_0_0_0.sql
@@ -1,37 +1,34 @@
-Let _Low_Risk_Filter_ = viewc() {
-AADRiskyUsers
-| where not(tostring(RiskLevel) has_any("medium", "hidden", "high", "none", "unknownFutureValue"))
-| where not tostring(RiskLevel) has_any("low")
-| where not(tostring(RiskState) has_any("none", "confirmedSafe", "remediated", "dismissed", "unknownFutureValue"))
-| where tostring(RiskState) has_any("atRisk", "confirmedCompromised")
-| where TimeGenerated > ago(30m) and TimeGenerated < ago(10m)
-| summarize count() by UserPrincipalName
-| take 1
+Let dataFiltrationStep1 = view() {
+    AADRiskyUsers
+    | where not(RiskLevel has_any("medium", "hidden", "high", "none", "unknownFutureValue"))
+    | where not (RiskLevel has_any("low"))
+    | where not(RiskState has_any("none", "confirmedSafe", "remediated", "dismissed", "unknownFutureValue"))
+    | where RiskState has_any("atRisk", "confirmedCompromised")
+    | where TimeGenerated > ago(30m) and TimeGenerated < ago(10m)
+    | summerise count() by UserPrincipalName
+    | take 1
 };
-
-Let _Medium_Risk_Filter_ = viewc() {
-AADRiskyUsers
-| where not(tostring(RiskLevel) has_any("low", "hidden", "high", "none", "unknownFutureValue"))
-| where not tostring(RiskLevel) has_any("medium")
-| where not(tostring(RiskState) has_any("none", "confirmedSafe", "remediated", "dismissed", "unknownFutureValue"))
-| where tostring(RiskState) has_any("atRisk", "confirmedCompromised")
-| where TimeGenerated > ago(30m) and TimeGenerated < ago(10m)
-| summarize count() by UserPrincipalName
-| take 1
+Let dataFiltrationStep2 = view() {
+  AADRiskyUsers
+  | where not(RiskLevel has_any("low", "hidden", "high", "none", "unknownFutureValue"))
+  | where not(RiskLevel has_any("medium"))
+  | where not(RiskState has_any("none", "confirmedSafe", "remediated", "dismissed", "unknownFutureValue"))
+  | where RiskState has_any("atRisk", "confirmedCompromised")
+  | where TimeGenerated > ago(30m) and TimeGenerated < ago(10m)
+  | summarize count() by UserPrincipalName
+  | take 1
 };
-
-Let _High_Risk_Filter_ = viewc() {
-AADRiskyUsers
-| where not(tostring(RiskLevel) has_any("medium", "hidden", "low", "none", "unknownFutureValue"))
-| where not tostring(RiskLevel) has_any("high")
-| where not(tostring(RiskState) has_any("none", "confirmedSafe", "remediated", "dismissed", "unknownFutureValue"))
-| where tostring(RiskState) has_any("atRisk", "confirmedCompromised")
-| where TimeGenerated > ago(30m) and TimeGenerated < ago(10m)
-| summarize count() by UserPrincipalName
-| take 1
+Let dataFiltrationStep3 = view() {
+  AADRiskyUsers
+  | where not(RiskLevel has_any("medium", "hidden", "low", "none", "unknownFutureValue"))
+  | where not (RiskLevel has_any("high"))
+  | where not(RiskState has_any("none", "confirmedSafe", "remediated", "dismissed", "unknownFutureValue"))
+  | where RiskState has_any("atRisk", "confirmedCompromised")
+  | where TimeGenerated > ago(30m) and TimeGenerated < ago(10m)
+  | summarize count() by UserPrincipalName
+  | take 1
 };
-
-let _Policy_Failure_Filter_ = view() {
+let dataFiltrationStep4 = view() {
     SigninLogs
     | mvexpand ConditionalAccessPolicies
     | where not(tostring(ConditionalAccessStatus) has_any("success", "notApplied"))
@@ -43,5 +40,4 @@ let _Policy_Failure_Filter_ = view() {
     | where CreatedDateTime > ago(30m) and CreatedDateTime < ago(10m)
     | project CreatedDateTime, UserPrincipalName, UserType, ResourceDisplayName, AuthenticationRequirement, AuthenticationDetails, IPAddress, DeviceDetail, LocationDetails, Location, Status, ConditionalAccessPolicies, ConditionalAccessStatus, RiskDetail
 };
-
-union withsource = "All Risky Users and Policy Failures" _Low_Risk_Filter_, _Medium_Risk_Filter_, _High_Risk_Filter_, _Policy_Failure_Filter_
+union withsource = "allRiskyUsers_and_policyFailures" dataFiltrationStep1, dataFiltrationStep2, dataFiltrationStep3, dataFiltrationStep4
