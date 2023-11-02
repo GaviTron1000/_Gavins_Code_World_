9b21f2b9319676919da4a954a1479a04d2f7bd29
GaviTron1000 <142179470+GaviTron1000@users.noreply.github.com>
2023-11-02 12:10:38 -0400

    For Specific users

---
+++ _Microsoft_Azure_Conditional_Access_Alerts_and_Reporting_/_Low_Risk_Only_/CA_LR_Alert_SU_V_1_0_0.sql
@@ -0,0 +1,23 @@
+let dataFiltrationStep1 = view() {
+    AADRiskyUsers
+    | where not(tostring(RiskLevel) has_any("medium", "high", "hidden", "none", "unknownFutureValue"))
+    | where tostring(RiskLevel) has_any("low")
+    | where not(tostring(RiskState) has_any("none", "confirmedSafe", "remediated", "dismissed", "unknownFutureValue"))
+    | where tostring(RiskState) has_any("atRisk", "confirmedCompromised")
+    | where tostring(UserPrincipalName) has_any("Username") //Fill in with the username that you are searching for EX: ASmith//
+    | summarize count() by UserPrincipalName
+    | take 1
+};
+let dataFiltrationStep2 = view() {
+    SigninLogs
+    | mvexpand ConditionalAccessPolicies
+    | where not(tostring(ConditionalAccessStatus) has_any("success", "notApplied"))
+    | where tostring(ConditionalAccessStatus) has_any("failure")
+    | where not(tostring(ConditionalAccessPolicies) has_any("success", "notApplied", "reportOnlyNotApplied"))
+    | where tostring(ConditionalAccessPolicies) has_any("failure")
+    | where not(Status has_any("successfully", "completed", "satisfied"))
+    | where not(AuthenticationDetails has_any("satisfied", "succeeded"))
+    | where tostring(UserPrincipalName) has_any("Username") //Fill in with the username that you are searching for EX: ASmith//
+    | project CreatedDateTime, UserPrincipalName, UserType, ResourceDisplayName, AuthenticationRequirement, AuthenticationDetails, IPAddress, DeviceDetail, LocationDetails, Location, Status, ConditionalAccessPolicies, ConditionalAccessStatus, RiskDetail
+};
+union withsource="Low Risk Users Report" dataFiltrationStep1, dataFiltrationStep2
